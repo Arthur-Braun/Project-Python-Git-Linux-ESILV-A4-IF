ab42e3e4c9ba3b5aacbd3bc3cc1a66b6b1916101
EC2 Default User <ec2-user@ip-172-31-46-177.eu-west-3.compute.internal>
2023-03-28 09:18:44 +0000

    Changes for daily report

--- scriptdash.py
+++ scriptdash.py
@@ -10,7 +10,6 @@ import pandas as pd
 import matplotlib
 
 
-
 #Extraire la data actualisée scrapée par le script bash
 
 with open('output.txt', 'r') as file:
@@ -32,8 +31,8 @@ with open(data_file, 'a') as f:
 #Récupération des données depuis le fichier pour créer un dataframe
 def load_data(file_path):
     df = pd.read_csv(file_path, sep=';', header=None)
-    df.columns = ['Time', 'EUR/KRW']
-    df['Time'] = pd.to_datetime(df['Time'])
+    df.columns = ['Time (UTC)', 'EUR/KRW']
+    df['Time (UTC)'] = pd.to_datetime(df['Time (UTC)'])
     return df
 
 #Calcul des données du daily report et mise en forme
@@ -42,10 +41,9 @@ def daily_report(df):
     low=today.min()
     high=today.max()
     mean=sum(today)/len(today)
-    variance=sum((mean-x) for x in today)/len(today)
-    volatility=(variance**(1/2))*100
-    ret=((today[len(today)-1]/today[0])-1)*100
-    return ("Daily low : " + str(round(low,2)) + ' | ' +  "Daily high : " + str(round(high,2))  + ' | ' +  "Daily mean : " + str(round(mean,2))  + ' | ' + "Daily volatility : " + str(round(volatility,2))  + ' % | ' + "Daily return : " + str(round(ret,2)) + ' %')
+    variance=sum((mean-x)**2 for x in today)/len(today)
+    volatility=(variance**(1/2))
+    return ("Daily low : " + str(round(low,2)) + ' | ' +  "Daily high : " + str(round(high,2))  + ' | ' +  "Daily mean : " + str(round(mean,2))  + ' | ' + "Daily volatility : " + str(round(volatility,2)) + " %")
 
 #Création du dashboard
 def create_app():
@@ -61,7 +59,7 @@ def create_app():
     @app.callback(Output('timeseries-plot', 'figure'), Input('interval-component', 'n_intervals'))
     def update_timeseries(n):
                 df = load_data('timeseries.txt')
-                fig = px.line(df, x='Time', y='EUR/KRW', title='EUR/KRW rate')
+                fig = px.line(df, x='Time (UTC)', y='EUR/KRW', title='EUR/KRW rate')
                 return fig
 
     @app.callback(Output('daily-report', 'children'), Input('daily-report-update', 'n_intervals'))
